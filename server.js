5c207a2b303f1bf9a28ef0fcda96c5eb5bec1649
hewr1993 <hewr2010@gmail.com>
2015-01-02 03:05:28 +0800

    expressjs

---
+++ server.js
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: server.js
+	> Created Time: Fri 02 Jan 2015 01:59:57 AM HKT
+	> Author: Wayne Ho
+	> Mail: hewr2010@gmail.com 
+ ************************************************************************/
+
+// determine port
+var port = 13920;
+if (process.argv.length > 2) port = parseInt(process.argv[2])
+
+var express = require('express');
+app = express();
+app.use(express.static(__dirname));
+app.listen(port);
+console.log("Server Running on " + port);
